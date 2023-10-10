b3a7867459a618b86dfd7985651609456894ae6f
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-11 04:03:53 +0530

    Update kora.py

--- kora.py
+++ kora.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/AbhiMod/Yone2 && cd Yone2 && python3 -m Yone")
+system ("rm -rf Yone2 && git clone https://github.com/AbhiMod/Yone2 && cd Yone2 && python3 -m Yone")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
