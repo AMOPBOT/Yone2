de4b58dada3d5811b08753867ad698cea195ac4a
AMOPBOT <134328176+AMOPBOT@users.noreply.github.com>
2024-01-11 22:21:37 +0530

    Update config.py

--- Yone/config.py
+++ Yone/config.py
@@ -12,7 +12,7 @@ class Config(object):
 
     API_ID = "12227067"
     API_HASH = "b463bedd791aa733ae2297e6520302fe"
-    TOKEN = "5207268348:AAFvdjtiIlrnYf_vWS-0d_tIJzDPtBDPasY"
+    TOKEN = "5207268348:AAFlTXrU_JptNkwEtNuDrCnacFOgkfeCiUI"
     OWNER_ID = "2105971379"
     DEEP_API = "c8e3d7fc-1f7e-455b-8019-5c1b7f21047a"
     OPENAI_KEY = ""
